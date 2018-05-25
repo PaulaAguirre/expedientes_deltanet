311f94d03355640c549f468881dfd9f3c98b3103
Paula Aguirre <pau88aguirre@gmail.com>
2018-05-25 17:27:05 -0400

    cambio de tamaño de logo

--- resources/views/index_exp.blade.php
+++ resources/views/index_exp.blade.php
@@ -6,7 +6,7 @@
     <br>
     <div class="row text-center">
         <div class="col-md-12">
-            <img src="{{('img/tranquility (3).png')}}" alt="Lights" style="width: 95%">
+            <img src="{{('img/tranquility (3).png')}}" alt="Lights" style="width: 80%">
 
         </div>
     </div>
